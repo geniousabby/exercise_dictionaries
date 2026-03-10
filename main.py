c65cd9b8cf9727c53b688cbc14714b4e75beddc4
geniousabby <abbyspam1228@gmail.com>
2026-03-10 12:59:15 -0700

    pseudo code

---
+++ main.py
@@ -0,0 +1,25 @@
+# Dictionary exercises
+
+# PETS:
+# Make 3 dictionaries where each dictionary represents a different pet.
+# In each dictionary, include the kind of animal and the owner's name.
+# Store these dictionaries in a list called "pets".
+# Next, loop through your list and as you do, print everything you know about each pet.
+# Make sure the final output is nicely formatted and neat.
+
+
+# PEOPLE:
+# Make 3 dictionaries representing different people (their firstname, lastname, and phone number)
+# and store all three dictionaries in a list called "people".
+# Loop through your list of people, printing everything you know about each person.
+# Make sure the final output is nicely formatted and neat.
+
+
+# CITIES
+# Make a dictionary called "cities".
+# Use the names of three cities as keys in your dictionary.
+# Create a dictionary of information about each city and include the country that the city is in,
+# its approximate population, and one fact about that city.
+# The keys for each city's dictionary should be something like "country, population, and fact".
+# Print the name of each city and all of the information you have stored about it.
+# Make sure the final output is nicely formatted and neat.
